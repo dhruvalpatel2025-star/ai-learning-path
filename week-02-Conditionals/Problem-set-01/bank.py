5c044d3575bce8935d709a409dc191b01f15fe5d
Dhruval Patel <dhruvalpatel2025@gmail.com>
2026-09-25 04:23:08 -0400

    problem-set-01

---
+++ week-02-Conditionals/Problem-set-01/bank.py
@@ -0,0 +1,8 @@
+greeting = input("Greeting: ")
+
+if 'Hello' in greeting:
+    print ("$0")
+elif 'H' in greeting:
+    print ("$20")
+else:
+    print("$100")
---
+++ week-02-Conditionals/Problem-set-01/extensions.py
@@ -0,0 +1,16 @@
+file_name = input ("File Nmae: ")
+
+if '.gif' in file_name:
+    print ("image/gif")
+elif '.jpg' in file_name or '.jpeg' in file_name:
+    print ("image/jpeg")
+elif '.png' in file_name:
+    print ("image/png")
+elif '.pdf' in file_name:
+    print ("application/pdf")
+elif '.txt' in file_name:
+    print ("text/plain")
+elif '.zip' in file_name:
+    print ("application/zip")
+else:
+    print ("application/octet-stream")
